ce7a6371d801a660702c84cbf4caa0ac2db8af3f
Fred <frederique.bordeleau@gmail.com>
2020-01-15 20:51:38 +0000

    Setup our stdin by creating setupIntput and handleUserInput functions

--- client.js
+++ client.js
@@ -12,9 +12,12 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('You successfully connected to the game server. Enjoy!');
   });
+  //When connected, client returns those two informations. Name + Move: up.
   conn.on('connect', () => {
     conn.write(`Name: FRE`);
-  }) 
+    //conn.write(`Move: down`);
+    //setInterval(() => {conn.write(`Move: left`)}, 50)
+  });
   //Handling event of client being rejected from server from idling
   conn.on('data', data => {
     console.log('you ded cuz you idled');
